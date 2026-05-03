4829a85cef0e1debc40490486d5317d40309925b
reoseah <reoseah9@gmail.com>
2026-05-03 23:08:26 +0200

    suspicious log

--- src/main/java/io/github/reoseah/utct/UTCT.java
+++ src/main/java/io/github/reoseah/utct/UTCT.java
@@ -12,9 +12,11 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.TintedParticleLeavesBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +24,7 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static net.minecraft.world.level.block.Blocks.leavesProperties;
+import static net.minecraft.world.level.block.Blocks.logProperties;
 
 public class UTCT implements ModInitializer {
     public static final String MOD_ID = "utct";
@@ -43,6 +46,12 @@ public class UTCT implements ModInitializer {
                         new TintedParticleLeavesBlock(0.01F, properties),
                 leavesProperties(SoundType.GRASS)
         );
+        public static final Block CHERRY_BRANCHES = register("cherry_branches", properties ->
+                        new TintedParticleLeavesBlock(0.01F, properties),
+                leavesProperties(SoundType.GRASS)
+        );
+        public static final Block SUSPICIOUS_CHERRY_LOG = register("suspicious_cherry_log", RotatedPillarBlock::new,
+                logProperties(MapColor.WOOD, MapColor.PODZOL, SoundType.WOOD));
 
         public static void initialize() {
         }
@@ -56,11 +65,15 @@ public class UTCT implements ModInitializer {
 
     public static class Items {
         public static final Item RED_CHERRY_LEAVES = register(Blocks.RED_CHERRY_LEAVES);
+        public static final Item CHERRY_BRANCHES = register(Blocks.CHERRY_BRANCHES);
+        public static final Item SUSPICIOUS_CHERRY_LOG = register(Blocks.SUSPICIOUS_CHERRY_LOG);
 
         private static void initialize() {
             CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.NATURAL_BLOCKS) //
                     .register(group -> {
                         group.accept(RED_CHERRY_LEAVES);
+                        group.accept(CHERRY_BRANCHES);
+                        group.accept(SUSPICIOUS_CHERRY_LOG);
                     });
         }
 
